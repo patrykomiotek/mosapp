aa9cd08bfbaab169aa5544641ad4c936b09ee4d2
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-23 07:21:25 +0200

    feat: storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        order: ["Atoms", "Molecules", "Organisms"],
+        method: "numeric",
+      },
+    },
+  },
+};
+
+export default preview;
