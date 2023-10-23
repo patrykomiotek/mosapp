79c455d10dddbc7c68bb44368aa3ad4166068350
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-23 12:20:19 +0200

    feat: component with state change

--- src/components/LoginFormWithState/LoginFormWithState.tsx
+++ src/components/LoginFormWithState/LoginFormWithState.tsx
@@ -1,31 +1,59 @@
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
+
+interface FormState {
+  email: string;
+  password: string;
+  language: string;
+}
 
 export const LoginFormWithState = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formState, setFormState] = useState<FormState>({
+    email: "",
+    password: "",
+    language: "",
+  });
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const id = event.target.id;
+    const value = event.target.value;
+    // structuredClone
+    setFormState({
+      ...formState,
+      [id]: id === "language" && value === "js" ? "***" : value,
+    });
+  };
 
   return (
     <div>
       <p>
-        E-mail: {email}, password: {password}
+        E-mail: {formState.email}, password: {formState.password}
       </p>
       <div>
-        <label>E-mail</label>
+        <label htmlFor="email">E-mail</label>
         <input
+          id="email"
           type="email"
-          onChange={(event) => setEmail(event.target.value)}
+          onBlur={handleChange}
+          value={formState.email}
         />
       </div>
       <div>
-        <label>Password</label>
+        <label htmlFor="password">Password</label>
         <input
+          id="password"
           type="password"
-          onChange={(event) => setPassword(event.target.value)}
+          onChange={handleChange}
+          value={formState.password}
         />
       </div>
       <div>
-        <label>Language</label>
-        <input type="text" />
+        <label htmlFor="language">Language</label>
+        <input
+          id="language"
+          type="text"
+          onChange={handleChange}
+          value={formState.language}
+        />
       </div>
       <button type="submit">Send</button>
     </div>
